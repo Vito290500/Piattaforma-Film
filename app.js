13b55eac6c57892d41ff103d153636ec5aa8685b
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2024-05-03 16:17:44 +0200

    added search function

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+//SECTION AND UL
 const SectionMain = document.getElementById('Section-Main');
-const ulSectionMain = document.querySelector('ul');
-const saveBtn = document.getElementById('add-film');
+const SectionResult = document.getElementById('resultSection');
+const ulSectionMain = SectionMain.querySelector('ul');
+const ulResultSection = SectionResult.querySelector('ul'); 
 
+//BUTTON
+const saveBtn = document.getElementById('add-film');
+const searchInput = document.getElementById('searchBar');
+const searchBtn = document.getElementById('searchBtn');
+const clearInputSearchBtn = document.getElementById('ClearInputSearchBtn');
+
+//INPUT FIELD
 const title = document.getElementById('title');
 const autore = document.getElementById('autore');
 const description = document.getElementById('description');
@@ -9,8 +18,9 @@ const durata = document.getElementById('durata');
 const tags = document.getElementById('tags');
 const imageUrl = document.getElementById('image-url');
 const filmUrl = document.getElementById('film-url');
+const SearchInputField = document.getElementById('searchBar');
 
-
+//CLASS FILM
 class FilmItem{
     constructor(
         Id, tittle, autore,
@@ -50,7 +60,32 @@ class FilmItem{
                 </div>`
         ulSectionMain.append(mainEl);
     }
+
+    renderResultSearchFilm(){
+        const resultEl = document.createElement('li');
+        resultEl.innerHTML = `   
+                <div class="dynamic-main-div" id="dynamic-main-div">
+                    <div class="background-image">
+                        <img src="${this.imageurl}" width="300px" height="400px">
+                    </div>
+                    <div class="content-card">
+                        <h3>${this.tittle}</h3>
+                        <div class="other-information">
+                            <p>${this.durata}</p>
+                            <p class="author">${this.autore}</p>
+                            <p>${this.tags}</p>
+                        </div>
+                        <p class="desc">${this.description.substring(0, 100) + '....'}</p>
+                        <div class="Tools">
+                            <button class="AddPrefer" id="Add-btn">+ Add To List</button>
+                            <button class="whatch"><a href="${this.url}">Watch Now -></a></button>
+                        </div>
+                    </div>
+                </div>`
+        ulResultSection.append(resultEl);
+    }
 };
+
 const RetriveFunctionItem = function(){
     const dbRequest = indexedDB.open('FilmList', 1);
 
@@ -132,9 +167,12 @@ const AddDataToDatabase = function(
             request.onerror = function(event) {
                 console.error("Error adding film:", event.target.error);
             };
-
+            
             request.onsuccess = function(event) {
                 console.log("Film added successfully!");
+                // Dispatch a custom event indicating that film is added successfully
+                const filmAddedEvent = new Event('filmAddedSuccessfully');
+                document.dispatchEvent(filmAddedEvent);
             };
         };
 
@@ -147,14 +185,94 @@ const AddDataToDatabase = function(
         const objectStore = db.createObjectStore('films', { keyPath: 'id' });
     };
 
-    location.reload();
+    
 };
+const HyperTextSearch = function(){
 
+    const inputValue = searchInput.value;
+    const dbRequest = indexedDB.open('FilmList', 1);
+
+    dbRequest.onsuccess = function(event){
+        const db = event.target.result;
+        
+        let filmFound = false;
+
+        const transaction = db.transaction(['films'], 'readwrite');
+        const objectStore = transaction.objectStore('films');
+
+        const getAllRequest = objectStore.getAll();
+
+        getAllRequest.onsuccess = function(event){
+            const allFilms = event.target.result;
+
+            for(const film of allFilms){    
+
+                //Successfully Findend the Film
+                if(inputValue.trim() == film.title){
+                    console.log('Film trovato');
+
+                    SectionMain.style.display = "none";
+                    ulSectionMain.style.display = "none";
+                    SectionResult.style.display = "block";
+                  
+                    const ResultItem = new FilmItem(
+                        film.id,
+                        film.title,
+                        film.autore,
+                        film.imageurl,
+                        film.description,
+                        film.url,
+                        film.durata,
+                        film.tags,
+                    );
+
+                    ResultItem.renderResultSearchFilm()
+                    filmFound = true;
+                    return;
+
+                //Suggestion findend films
+                }else if(film.title.includes(inputValue.trim())){
+
+                    console.log('Film trovato (suggestion): ' + film.title)
+                    
+                    SectionMain.style.display = "none";
+                    ulSectionMain.style.display = "none";
+                    SectionResult.style.display = "block";
+                  
+                    const ResultItem = new FilmItem(
+                        film.id,
+                        film.title,
+                        film.autore,
+                        film.imageurl,
+                        film.description,
+                        film.url,
+                        film.durata,
+                        film.tags,
+                    );
+                    ResultItem.renderResultSearchFilm()  
+                    filmFound = true;
+                    return;
+                }
+            //No film Findend error
+            }if(!filmFound){  
+                const modalError = document.getElementById('modal-error');
+                const backgroundSurround = document.getElementById('background-surround');
+                modalError.style.display = 'block';
+                backgroundSurround.style.display = 'block';
+            }
+        }
+        getAllRequest.onerror = function(event){
+            alert("Nessun film trovato!")
+        }
+    }
+    dbRequest.onerror = function(event){
+        console.log("Ops, something went wrong with database!")
+    }
+}
 
 RetriveFunctionItem();
 
-saveBtn.addEventListener('click', () =>{
-
+saveBtn.addEventListener('click', () => {
     AddDataToDatabase(
         titleInput = title.value,
         autoreInput = autore.value,
@@ -164,10 +282,28 @@ saveBtn.addEventListener('click', () =>{
         urlInput = filmUrl.value,
         tagsInput = tags.value
     );
+});
 
-})  
+searchBtn.addEventListener('click', () =>{
+    HyperTextSearch();
+})
 
+clearInputSearchBtn.addEventListener('click', () =>{
+    SectionMain.style.display = "block";
+    ulSectionMain.style.display = "flex";
+    SectionResult.style.display = "none";
+    ulResultSection.style.display = "none";
 
+    searchBar.value = '';
+
+    location.reload()
+
+})
+
+document.addEventListener('filmAddedSuccessfully', () => {
+    console.log("Film added successfully!");
+    location.reload();
+});
 
 
 
--- design-app.js
+++ design-app.js
@@ -3,7 +3,10 @@ const modal = document.getElementById('modal-section');
 const background = document.getElementById('background-surround');
 const clearbtn = document.getElementById('clear-btn');
 const input = document.querySelectorAll("input");
-const textarea = document.querySelector("textarea")
+const textarea = document.querySelector("textarea");
+const SearchInput = document.getElementById('searchBar');
+const clearInputSearch = document.getElementById('ClearInputSearchBtn');
+const modalError = document.getElementById('modal-error');
 
 addbtn.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -11,6 +14,7 @@ addbtn.addEventListener('click', () => {
 })
 
 background.addEventListener('click', () =>{
+    modalError.style.display = 'none';
     modal.style.display = 'none';
     background.style.display = 'none';
 })
@@ -21,3 +25,13 @@ clearbtn.addEventListener('click', () => {
     });
     textarea.value = '';
 });
+
+SearchInput.addEventListener('click', () =>{
+    clearInputSearch.style.display = 'block'
+})
+
+clearInputSearch.addEventListener('click', () => {
+    clearInputSearch.style.display = 'none';
+});
+
+
